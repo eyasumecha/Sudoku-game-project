353f424feedbe8ea4742ddd2998e00df1712fb91
Wending Wu <jajefan@moose.cs.dartmouth.edu>
2020-06-01 00:13:43 -0400

    Reorganized project structure to fit requirements (main is now a separate module, completed), and created empty files (TODO) as reminders of dependencies.

---
+++ creator/creator.c
@@ -0,0 +1,20 @@
+/**
+ * Eyasu Lemma, Pratinav Bagla, and Wendell Wu
+ * CS50 Final Project - Sudoku Creator Module
+ * 5/31/20
+ * 
+ * This module is responsible for creating a new randomized sudoku puzzle which
+ * only has 1 unique solution and has a minimum of 40 numbers removed.
+ */
+
+
+/**
+ * This function is called by program execution
+ *  if "create" was called. It takes in no parameters
+ *  and does not return anything. It simply creates
+ *  a puzzle guaranteed to have a unique solution and
+ *  prints it to stdout.
+ */
+void create_puzzle(){
+
+}
---
+++ run/run.c
@@ -0,0 +1,35 @@
+/**
+ * Eyasu Lemma, Pratinav Bagla, Wendell Wu
+ * CS50 Final Project - Main Function
+ * 
+ * This is the module which parses parameters and calls the appropriate
+ * modules to either solve or create a puzzle.
+ */
+
+#include <string.h>
+
+//include other modules
+#include "common.h"
+#include "creator.h"
+#include "solver.h"
+
+
+int main(int argc, char *argv[]){
+    const char *USAGE = "Usage: sudoku <create/solve>\nExiting...\n";
+    
+    if(argc != 2){
+        fprintf(stderr, "Wrong number of arguments.\n%s", USAGE);
+        return 1;
+    }
+
+    if(strcmp(argv[1], "create") == 0){
+        create_puzzle();
+    }else if(strcmp(argv[1], "solve") == 0){
+        //call the solver algorithm
+    }else{
+        fprintf(stderr, "Argument was not create or solve.\n%s", USAGE);
+        return 2;
+    }
+
+    return 0;
+}
