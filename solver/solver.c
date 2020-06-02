934bc36eae5249eb8771543eaafe1a3021a68dcd
Pratinav Bagla <pratinavbagla@gmail.com>
2020-06-02 23:44:34 +0530

    solver: Clean, fix, move all arrays to stack

--- solver/solver.c
+++ solver/solver.c
@@ -7,36 +7,24 @@
 
 #include "common.h"
 
-void solver(){
-    int **sudoku = (int **)malloc(9 * sizeof(int*));
-    for(int i = 0; i < 9; i++){
-        sudoku[i] = (int *)malloc(9 * sizeof(int));
-    }
+void sudoku_solver()
+{
+    int sudoku[9][9]= {0};
     int value;
-    int x =0, y = 0;
-    while (scanf("%d", &value) != EOF && x < 9){
-       sudoku[x][y] = value;
-       y++;
-       if(y == 9){
-           y = 0;
-           x++;
+    int row = 0, col = 0;
+
+    while (scanf("%d", &value) != EOF && row < 9) {
+       sudoku[row][col] = value;
+       if (++col >= 9) {
+           col = 0;
+           row += 1;
        }
     }
     
     // check if the input puzzle was correctly parsed.
-    if(x != 9){
+    if (row != 9) {
         fprintf(stderr, "Input sudoku was of incorrect format. Exiting...\n");
-        for(int i = 0; i < 9; i++){
-            free(sudoku[i]);
-        }
-        free(sudoku);
         return;
-    }
-
-    //create some memory to hold the solution
-    int **solution = (int **) calloc(9, sizeof(int *));
-    for(int i = 0; i < 9; i++){
-        solution[i] = (int *) calloc(9, sizeof(int));
     }
 
     #ifdef DEBUG
@@ -44,27 +32,19 @@ void solver(){
         print_sudoku(sudoku);
     #endif
 
+    int solution[9][9] = {0};
     int ret = unique_solver(sudoku, solution);
     
     #ifdef DEBUG
         printf("\nSolved sudoku:\n");
     #endif
 
-    if(ret == 0){
+    if (ret == 0) {
         printf("No solutions found.\n");
-    }else{
+    } else {
         print_sudoku(solution);
-        if(ret == 2)
+        if (ret == 2)
             printf("Multiple solutions found.\n");
-    }
-    
-    //clean up memory
-    for(int i = 0; i < 9; i++){
-        free(solution[i]);
-    }
-    free(solution);
-    for(int i = 0; i < 9; i++){
-        free(sudoku[i]);
-    }
-    free(sudoku);
+    } 
 }
+
--- solver/solver.h
+++ solver/solver.h
@@ -1,9 +1,11 @@
 #ifndef __SOLVER_H_
 #define __SOLVER_H_
 
-/**
+/**************** sudoku_solver ****************/
+/*
  * Called by sudoku when solve is the argument passed.
  */
-void solver();
+void sudoku_solver();
 
 #endif
+
